b9e5e879ab551437e801887340fc0b4eb2d75f5e
Omer Ozdemir <oomerozdemir40@gmail.com>
2025-11-25 16:26:27 +0300

    Initial backend commit

--- src/index.js
+++ src/index.js
@@ -20,10 +20,23 @@ import contactRoutes from "./routes/contactRoutes.js"
 dotenv.config();
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "http://localhost:3000",
+  process.env.FRONTEND_URL 
+];
+
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"],
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 }));
+
 app.use(cookieParser());        
  app.use(express.json());
 
